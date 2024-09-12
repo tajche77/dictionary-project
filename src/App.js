6fd0eee9d47c94257667444f790ba89911342bf8
tajche77 <bozic.tamara@gmail.com>
2024-09-12 16:09:44 +0200

    initial commit - added bootstrap and imported logo

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import logo from "./logo.png";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo img-fluid" alt="logo" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
