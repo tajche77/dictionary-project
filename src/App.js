6665be9ea2f6b7665418d8c711c6dd68fd47d962
tajche77 <bozic.tamara@gmail.com>
2024-09-12 16:59:31 +0200

    added component Dictionary

--- src/App.js
+++ src/App.js
@@ -1,12 +1,47 @@
 import "./App.css";
 import logo from "./logo.png";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo img-fluid" alt="logo" />
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>
+            This project was coded by
+            <a
+              href="https://github.com/tajche77"
+              target="_blank"
+              rel="noreferrer"
+            >
+              {" "}
+              TamaraB
+            </a>
+            , is open-sourced on {""}
+            <a
+              href="https://github.com/tajche77/dictionary-project"
+              target="_blank"
+              rel="noreferrer"
+            >
+              GitHub {""}
+            </a>
+            and is hosted on {""}
+            <a
+              href="https://zippy-centaur-b67eef.netlify.app/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Netlify
+            </a>
+          </small>
+        </footer>
+      </div>
     </div>
   );
 }
